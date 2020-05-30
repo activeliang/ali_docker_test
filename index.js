c22e8e445f5ff3690be5570325d52f24f90c1c99
activeliang <activeliang@gmail.com>
2020-05-30 17:40:57 +0800

    test

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 const axios = require('axios');
 
+console.log('start! yeah!')
 axios.get('http://pt.hongliang.fun?from=ali_docker_test').then(res => console.log(res))
+
+console.log('close')
+console.log('close')
+console.log('close')
+console.log('close')
